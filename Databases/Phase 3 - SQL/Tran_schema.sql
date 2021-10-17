f8cc31f641b78f6d0c2e3d336ea11e4bc22d9d80
Thien Tran <19trant7@gmail.com>
2021-10-17 00:23:18 -0500

    Added DDL constraints

--- Databases/Phase 3 - SQL/Tran_schema.sql
+++ Databases/Phase 3 - SQL/Tran_schema.sql
@@ -6,24 +6,26 @@ DROP TABLE Agent;
 
 CREATE TABLE Seller(
     SocialSecurityNumber NUMBER(9),
-    SellerName VARCHAR(34),
-    PhoneNumber NUMBER(10),
+    SellerName VARCHAR(34) NOT NULL,
+    PhoneNumber NUMBER(10) NOT NULL,
     SpouseName VARCHAR(34),
     CONSTRAINT SellerPk PRIMARY KEY(SocialSecurityNumber)
 );
 
 CREATE TABLE Agent(
     AgentID CHAR(8),
-    AgentName VARCHAR(34),
-    OfficeName VARCHAR(20),
+    AgentName VARCHAR(34) NOT NULL,
+    OfficeName VARCHAR(20) NOT NULL,
     CONSTRAINT AgentPk PRIMARY KEY(AgentID)
 );
 
 CREATE Table House(
     HomeID CHAR(7),
-    StreetAddress VARCHAR(28),
-    StateName VARCHAR(14),
-    SquareFeet NUMBER(6),
+    StreetAddress VARCHAR(28) UNIQUE NOT NULL,
+    StateName VARCHAR(14) NOT NULL,
+    SquareFeet NUMBER(6) NOT NULL,
+    AskingPrice NUMBER(10) NOT NULL,
+    CommisionPercent NUMBER(3) NOT NULL,
     SellerID NUMBER(9),
     AgentID CHAR(8),
     CONSTRAINT HousePk PRIMARY KEY(HomeID),
@@ -43,10 +45,10 @@ CREATE TABLE AgentPhone(
 
 CREATE TABLE Buyer(
     SocialSecurityNumber NUMBER(9),
-    BuyerName VARCHAR(34),
-    PhoneNumber NUMBER(10),
-    LowerPriceLimit NUMBER(10),
-    UpperPriceLimit NUMBER(10),
+    BuyerName VARCHAR(34) NOT NULL,
+    PhoneNumber NUMBER(10) NOT NULL,
+    LowerPriceLimit NUMBER(10) NOT NULL,
+    UpperPriceLimit NUMBER(10) NOT NULL,
     AgentID CHAR(8),
     CONSTRAINT BuyerPk PRIMARY KEY(SocialSecurityNumber),
     CONSTRAINT AgentRepresentingFk FOREIGN KEY(AgentID)

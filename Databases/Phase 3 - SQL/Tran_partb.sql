acfa42c89fd6f15adceb1ad6d843614921be5a0b
Thien Tran <19trant7@gmail.com>
2021-10-17 20:20:22 -0500

    Finished up to #7 of Part B

--- Databases/Phase 3 - SQL/Tran_partb.sql
+++ Databases/Phase 3 - SQL/Tran_partb.sql
@@ -7,6 +7,10 @@ SELECT * FROM Buyer
 WHERE AgentID = 'PL1W980' --Default to Agent PL1W980 instead of 1 since my AgentID is complexer than a number.
 ORDER BY UpperPriceLimit ASC;
 
+SELECT * FROM Buyer
+WHERE AgentID = '1' --Version with strict requirement.
+ORDER BY UpperPriceLimit ASC;
+
 --3. Find the names of all buyers who have a phone number with a 504 area code (hint: remember the LIKE operator).
 SELECT BuyerName FROM Buyer
 WHERE PhoneNumber LIKE '504%';
@@ -20,6 +24,12 @@ SELECT SquareFeet FROM House
 WHERE AskingPrice BETWEEN 100000 AND 200000;
 
 --6. Find all buyers who have a phone number with a 312 area code and do not have agent ID 1.
+SELECT * FROM Buyer
+WHERE PhoneNumber LIKE '312%' AND AgentID <> 'PL1W980'; --Default to Agent PL1W980 instead of 1 since my AgentID is complexer than a number.
+
+SELECT * FROM Buyer
+WHERE PhoneNumber LIKE '312%' AND AgentID <> '1'; --Version with strict requirement.
+
 --7. Find the minimum and maximum square feet for all houses. Use only one query.
 --8. Find the average max price for all buyers with agent ID 1. 
 --9. Find the house with the highest price. Do not hardcode any salaries or other values.

30c00bbc2d88331b772032e4173ce60d965a3b33
Thien Tran <19trant7@gmail.com>
2021-10-29 16:50:04 -0500

    Setup problems

---
+++ Databases/Phase 4 - Intermediate SQL/Tran_phase4.sql
@@ -0,0 +1,31 @@
+--1. Retrieve the street address for houses with an agent in the New Orleans office. 
+
+--2. Retrieve the street address for house which have a seller name that is the same as the listing agent’s name. 
+
+--3. Find names of all agents who represent a buyer with a minimum price range greater than $80K and a maximum price range less than $225K. 
+
+--4. For each agent, list their name, office, and the total number of buyers they represent. 
+
+--5. Retrieve the street address for all houses that have an agent who is representing at least one buyer. 
+
+--6. Retrieve the street address for all houses that have an agent who is not representing any buyers. 
+
+--7. For each agent, retrieve the agent’s name and the average commission of all houses they are listing. 
+
+--8. Retrieve the average price for all houses in the state of Louisiana. 
+
+--9. List the names of all agents and the number of phone numbers they have. 
+
+--10. Find the names of all agents who represent exactly two buyers. 
+
+--11. For each agent whose average commission is greater than $10K, retrieve the agent’s name and the number houses they represent. 
+
+--12. Retrieve the names of all buyers who are represented by the agent who is listing the lowest priced house. 
+
+--13. Retrieve the agent’s name and the buyer’s name for all agents who are listing a house within the buyer’s price range (i.e., house price is between minimum and maximum price range). 
+
+--14. Find sellers whose SSN number has a pattern 321 repeated twice (sequentially). For example, the query should return sellers with the SSN’s ‘321-32-145’ and ‘983-21-3219’, but not ‘321-99-3218’. 
+
+--15. Find agents whose office consists of exactly 2 words. For example, the query should return records for “Metairie Office” and “Downtown Office”, but not “University of New Orleans Office”. 
+
+--16. Find all houses that include a street number in the street address. For example, the query should return records for “2000 Lakeshore Drive” and “1500 Sugar Bowl Drive” but not “Canal Street”. 

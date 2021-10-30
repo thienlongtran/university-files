2a6735d2ce2e100e05ce325750508030b06011a8
Thien Tran <19trant7@gmail.com>
2021-10-30 16:31:08 -0500

    Completed beginner's practice set

--- Databases/Practice Set - Beginner/answers.sql
+++ Databases/Practice Set - Beginner/answers.sql
@@ -25,21 +25,41 @@ SELECT AVG(P_PRice) FROM Part
 WHERE P_Color = 'Green';
 
 -- Find all customers that have the same name as a supplier
+SELECT * FROM Customer
+WHERE C_NAME IN (SELECT S_NAME from Supplier);
 
 -- Find all orders in the year 2014 that were shipped by boat
+SELECT * FROM Orders
+WHERE O_Date LIKE '%14' AND O_Shipmode = 'Boat';
 
 -- Find all parts related to Dr. Jones
+SELECT * FROM Part
+WHERE P_Name LIKE 'Dr. Jones %';
 
 -- Find all of the parts that have a size less than 30 and order the output by color.
+SELECT * FROM Part
+WHERE P_Size < 30
+ORDER BY P_Color;
 
 -- Find the prices and colors of all Gremlins
+SELECT P_Price, P_Color FROM Part
+WHERE P_Name = 'Gremlin';
 
 -- Find all parts that are larger than the average size
+SELECT * FROM Part
+WHERE P_Size > (SELECT AVG(P_Size) FROM Part);
 
 -- Find the number of orders place by customer 1
+SELECT * FROM Orders
+WHERE O_CustID = 1;
 
 -- Find the maximum price among all red parts
+SELECT MAX(P_Price) FROM Part
+WHERE P_Color = 'Red';
 
 -- Find all colors for Reese's Pieces 
+SELECT P_Color AS Color FROM Part
+WHERE P_Name = 'Reese''s Pieces';
 
 -- Find orders that have been repeated by the same supplier and customer pairs
+SELECT O_CustID, O_SuppID, O_PartID, COUNT(*) FROM Orders

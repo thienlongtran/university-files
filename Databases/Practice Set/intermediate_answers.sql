2e85f7859d71583fcf9724dc194c4d23b288ef2d
thienlongtran <tltran5@uno.edu>
2021-11-04 17:32:31 -0500

    Started intermediate questions on practice set

---
+++ Databases/Practice Set/intermediate_answers.sql
@@ -0,0 +1,57 @@
+-- Find the number of customers for each city
+
+-- Find the total price for all orders
+
+-- Find the city with the most customers
+
+-- Find all suppliers that live in a city that has customers
+
+-- Find all supplier that live in a city without customers
+
+-- Find the average size and price for each part name
+
+-- Find the most common part color
+
+-- Find all shipping modes for Gremlins
+
+-- Find the most common shipping mode for Gremlins
+
+-- Find the customer who has ordered the most Gremlins
+
+-- Find the supplier who has fulfilled the most orders
+
+-- Find the city that ships the most parts by Boat to a customer's city
+
+-- Find the average part size for each shipping mode
+
+-- Find all customers who purchased a Gremlin
+
+-- Find all parts with the same size-- Find the number of customers for each city
+
+-- Find the city with the most customers
+
+-- Find the most common part color
+
+-- Find all suppliers that live in a city that has customers
+
+-- Find all supplier that live in a city without customers
+
+-- Find the average size and price for each part name
+
+-- Find all shipping modes for Gremlins
+
+-- Find the most common shipping mode for Gremlins
+
+-- Find the customer who has ordered the most Gremlins
+
+-- Find the supplier who has fulfilled the most orders
+
+-- Find the city that ships the most parts by Boat to a customer's city
+
+-- Find the average part size for each shipping mode
+
+-- Find the total price for all orders
+
+-- Find all customers who purchased a Gremlin
+
+-- Find all parts with the same size
